23df4657e3ab3d66d7babf2256aa37f4a18a86f0
antho <anthonyjrchen@gmail.com>
2023-10-21 19:34:48 -0700

    adding m

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,18 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "/Dashmote-Frontend-Case/",
+  // disable hashes in filenames
+  filenameHashing: false,
+  // delete HTML related webpack plugins
+  chainWebpack: config => {
+    config.plugins.delete('html')
+    config.plugins.delete('preload')
+    config.plugins.delete('prefetch')
+  },
+  build: {
+    rollupOptions: {
+      // overwrite default .html entry
+      input: 'E:/Downloads/Dashmote-Frontend-Case-master/Dashmote-Frontend-Case/src/App.vue',
+    },
+  }
 })
