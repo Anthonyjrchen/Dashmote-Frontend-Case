ca0a36ed2256bf3c29f48b1206de3be47af5be16
antho <anthonyjrchen@gmail.com>
2023-10-21 18:37:07 -0700

    trying something

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  base: "dashcase",
+  publicPath: "/Dashmote-Frontend-Case/",
 })
